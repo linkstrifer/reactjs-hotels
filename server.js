3c1cbfc5e84d014ccca5a589a66fc77dffb8f79d
Link Strifer <linkstrifer@outlook.com>
2018-01-18 18:15:50 -0500

    fix CORS

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const Routes = require('./api/hotels/hotels.routes');
 const App = express();
 const port = process.env.PORT || 8010;
 
+App.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 Routes(App);
 
 App.use((req, res) => {
